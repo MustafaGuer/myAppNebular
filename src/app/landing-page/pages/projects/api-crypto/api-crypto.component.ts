ba4f5dbf7d05146473394dce686bf587f8769c20
Mustafa.Guer <mustafa.guer@trans4mation.de>
2022-04-30 21:23:45 +0200

    chore: update sec, add vars

--- src/app/landing-page/pages/projects/api-crypto/api-crypto.component.ts
+++ src/app/landing-page/pages/projects/api-crypto/api-crypto.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, map, Observable, tap } from 'rxjs';
 
 import { CURRENCIES } from 'src/app/shared/consts/currencies';
+import { CRYPTO_LINKS } from 'src/app/shared/consts/crypto-compare-links';
 
 @Component({
   selector: 'app-api-crypto',
@@ -10,14 +11,13 @@ import { CURRENCIES } from 'src/app/shared/consts/currencies';
   styleUrls: ['./api-crypto.component.scss'],
 })
 export class ApiCryptoComponent implements OnInit {
-  minApiUrlSingleData: string =
-    'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR';
-  minApiUrlMultipleData: string =
-    'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR';
-  minApiUrlCompleteData: string =
-    'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD,EUR';
+  minApiUrlSingleData: string = CRYPTO_LINKS.single_data;
+  minApiUrlMultipleData: string = CRYPTO_LINKS.multiple_data;
+  minApiUrlCompleteData: string = CRYPTO_LINKS.complete_data;
+
   btcInCurrencies: any = {};
   btcInYen: number = 0;
+  btcInDollar: number = 0;
   datetime: number = Date.now();
 
   currencies = CURRENCIES;
@@ -25,17 +25,28 @@ export class ApiCryptoComponent implements OnInit {
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.datetime = Date.now();
+    }, 1000);
+    this.getSingleDataFromCryptoCurrency();
+    setInterval(() => {
+      this.getSingleDataFromCryptoCurrency();
+    }, 20000);
+  }
+
+  private handleError(): any {}
+
+  getSingleDataFromCryptoCurrency() {
     this.httpClient
       .get(this.minApiUrlSingleData)
       .pipe(
         catchError(this.handleError()),
         map((data) => {
           this.btcInCurrencies = data;
-
           this.btcInYen = data['JPY'];
+          this.btcInDollar = data[this.currencies.Dollar];
         })
       )
       .subscribe();
   }
-  private handleError(): any {}
 }
---
+++ src/app/shared/consts/crypto-compare-links.ts
@@ -0,0 +1,5 @@
+export const CRYPTO_LINKS = {
+  single_data: 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR',
+  multiple_data: 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR',
+  complete_data: 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD,EUR',
+}
